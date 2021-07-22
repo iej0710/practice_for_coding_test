b0054083d8bb90c23659769d10b91eadb5c72b37
iej0710 <iej0710@dgist.ac.kr>
2021-07-22 15:47:43 +0900

    commit message

---
+++ baekjoon/stack/10773.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+arr = []
+for i in range(N):
+    x = int(input())
+    if x == 0:
+        arr.pop()
+    else:
+        arr.append(x)
+
+print(sum(arr))
---
+++ baekjoon/stack/9012.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+arr = []
+for i in range(T):
+    flag = 1
+    s = input()
+    for j in s:
+        if j == '(':
+            arr.append(j)
+        elif j == ')' and len(arr) > 0:
+            arr.pop()
+        elif j == ')' and len(arr) == 0:
+            flag = 0
+            break
+    if len(arr) == 0 and flag == 1:
+        print('YES')
+    else:
+        print('NO')
