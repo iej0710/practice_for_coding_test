2ee93e05aa172718a20a0e731f5a8060b55744cd
iej0710 <iej0710@dgist.ac.kr>
2021-07-22 16:37:43 +0900

    commit message

--- baekjoon/stack/4949.py
+++ baekjoon/stack/4949.py
@@ -4,28 +4,31 @@ while 1:
     except:
         break
     if len(s) == 1 and s[0] == '.':
-        print('yes')
         break
     stack1 = []
-    stack2 = []
+
     flag = 1
     for char in s:
-        if char == '[':
+        if char == '[' or char == '(':
             stack1.append(char)
-        elif char == '(':
-            stack2.append(char)
         elif char == ']' and len(stack1) > 0:
-            stack1.pop()
-        elif char == ')' and len(stack2) > 0:
-            stack2.pop()
+            tmp = stack1.pop()
+            if tmp == '(':
+                flag = 0
+                break
+        elif char == ')' and len(stack1) > 0:
+            tmp = stack1.pop()
+            if tmp == '[':
+                flag = 0
+                break
         elif char == ']' and len(stack1) == 0:
             flag = 0
             break
-        elif char == ')' and len(stack2) == 0:
-            flag == 0
+        elif char == ')' and len(stack1) == 0:
+            flag = 0
             break
 
-    if flag == 1 and len(stack1) == 0 and len(stack2) == 0:
+    if flag == 1 and len(stack1) == 0:
         print('yes')
     else:
         print('no')
