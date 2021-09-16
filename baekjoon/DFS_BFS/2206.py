9f3d0fa0fc9b5fb99aaf8cef01636fe3d2046d00
iej0710 <iej0710@dgist.ac.kr>
2021-09-16 18:29:26 +0900

    breadth first search

--- baekjoon/DFS_BFS/2206.py
+++ baekjoon/DFS_BFS/2206.py
@@ -1,4 +1,3 @@
-import sys
 from collections import deque
 
 n, m = map(int,input().split())
@@ -8,11 +7,11 @@ for y in range(n):
     mat.append(list(map(int,input())))
 
 visited = [[[0 for i in range(m)] for j in range(n)] for k in range(2)]
-process = deque([[0,0,1]])
+process = deque([[0,0,0]])
 flag = 0
 delta_x = [1, -1, 0, 0]
 delta_y = [0, 0, 1, -1]
-visited[1][0][0] = 1
+visited[0][0][0] = 1
 while len(process) > 0:
     y,x,count = process.popleft()
     if x == m - 1 and y == n - 1:
@@ -23,9 +22,9 @@ while len(process) > 0:
         idx = x + dx
         idy = y + dy
         if 0 <= idx < m and 0 <= idy < n:
-            if mat[idy][idx] == 1 and count == 1:
-                visited[0][idy][idx] = visited[1][y][x] + 1
-                process.append([idy,idx,0])
+            if mat[idy][idx] == 1 and count == 0:
+                visited[1][idy][idx] = visited[0][y][x] + 1
+                process.append([idy,idx,1])
             elif mat[idy][idx] == 0 and visited[count][idy][idx] == 0:
                 visited[count][idy][idx] = visited[count][y][x] + 1
                 process.append([idy,idx,count])
